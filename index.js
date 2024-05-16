b7a3b801aa19527e9b236c68d66c99ba98e3eacf
ShaziaMahar <shaziamahar478@gmail.com>
2024-05-16 11:21:45 +0500

    commit1

---
+++ index.js
@@ -0,0 +1,98 @@
+// let num = 77;
+// num = 776;
+// num = 88;
+// num = 33;
+// console.log(num);
+// const num = 77;
+
+
+// let num = "44";
+// let num2 = 66;
+// alert(num + num2);
+// console.log(num + num2); 
+// // number + number addition
+// // num + string concatenation
+// // string + number concatenation
+// let firstname = "ali";
+// let lastname = "shuhab"
+// console.log(firstname  + "" + lastname);
+
+// let email = "asd@gmail.com";
+// let name = "shazia";
+// let account = 7652455425;
+// let etc
+// console.table([email, name account, etc]);
+// // math operators = + - * / divided % modules
+// let num = "66";
+// let num2 = 1;
+// console.log(num + num2);
+// let data = "100";
+// let data2 = 5;
+// console.log(data * data2);
+
+// let data = "100";
+// let data2 = 5;
+// console.log(data / data2);
+
+// let data = "10";
+// let data2 ="3";
+// let add = data % data2;
+// console.log(add);
+
+// let data = "11";
+// let data2 = "3";
+// let add = data % data2;
+// console.log(add);
+
+
+
+
+// let name = prompt(" what is your name");
+
+// console.log(name);
+// document.write(name);
+
+// let name = prompt(" what is your name");
+// console.log(name);
+
+// let rendomNumber = Math.rendom();
+// console.log(rendomNumber1);
+
+// let rendomNumber = Math.rendom();
+// rendomNumber = rendomNumber * 10;
+// let rounded = Math.round(rendomNumber);
+// console.log(rounded);
+
+
+
+// BODMAS= BAREK OPEN DIVIDED MULTPLY ADD SUBTRAIC
+// let num =   4 + 6 * (5 + 5);
+// console.log(num);
+
+// let x = 33;
+// console.log(x);
+// console.log(typeof String(x));
+
+
+
+// let name =
+// console.log(name);
+
+// let name = null;
+// console.log(name);
+
+// Boolean
+// let x  = 33;
+// let y = true;
+// console.log(typeof y);
+
+// console.log(x + y);
+// true vale=1
+// false value=0
+
+// let x = 33;
+// let  y = true;
+// console.log(x + 12 y);
+
+
+// github.com/shoaib420/javascript-classes
