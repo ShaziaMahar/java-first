a8ea5af381fa2c85d0be488b8772f0e37729328d
ShaziaMahar <shaziamahar478@gmail.com>
2024-05-22 09:27:42 +0500

    commit8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
- let num = 77;
- let num2 = 2;
- console.log(num + num2);
+//  let num = 77;
+//  let num2 = 2;
+//  console.log(num + num2);
 
 
 
@@ -100,7 +100,173 @@
 
 // let x = 33;
 // let  y = true;
-// console.log(x + 12 y);
+// console.log(x + 12 + y);
+
+// math operators = + - * / divided % modules;
+//  let num = "66";
+//  let num2 = 1;
+//  console.log(num + num2);
+//  let data = "100";
+//  let data2 = 5;
+//  console.log(data * data2);
+
+// let name = null;
+// console.log(name);
+
+// let name 
+// console.log(name);
 
 
-// github.com/shoaib420/javascript-classes
+
+// let rendomNumber2 = Math.rendom();
+// rendomNumber2 = rendomNumber2 * 10;
+// console.log(rendomNumber2);
+
+
+//  let rendomNumber = Math.rendom();
+//   console.log(rendomNumber);
+
+
+// let num =   4 + 6 * (5 + 5);
+//  console.log(num);
+
+
+
+
+// let data = "10";
+//  let data2 ="3";
+//  let add = data % data2;
+//  console.log(add);
+
+
+//  let data = "100";
+//   let data2 = 5;
+//   console.log(data / data2);
+//  hhhhhhhhhhhhhhhhh
+//  comperision operators
+// == ===
+// let num1 = 6;
+// let num2 = 6;
+// console.log(num1 == num2);
+
+// let num1 = 6;
+//  let num2 = 6 ;
+//  console.log(num1 === num2);
+
+
+// let num1 = 6;
+//  let num2 ="6" ;
+//  console.log(num1 == num2);
+
+
+// != ,!` ==
+// let num1 = 6;
+//  let num2 = "6" ;
+//  console.log(num1 !== num2);
+// console.log("5" !== 5)
+
+
+
+/* < greater Then
+> lessthan
+console.log(5 > 7); */
+
+// let num = 55;
+// let num2 = 66;
+// console.log(num < num2);
+
+
+// <=  <==
+// greater the double equal
+// let num = 155;
+// let num2 = 66;
+// console.log(num >= num2);
+
+
+// let num = 155;
+// let num2 = 9999;
+// console.log(num <= num2);
+
+
+// condition
+// let age = +prompt("inter your age");
+// if (age >= 18) {
+//     console.log("you can apply cnic");
+// } else {
+//     console.log("you are not eligble");
+// }
+
+
+//  let userNum = +prompt("inter your name");
+//  let computerNumber = Matth.round( Math.random() * 5 );
+
+//  if( userNum ===  computerNumber){
+//      console.log(" you win");
+//  }else{
+//      console.log(" you loss !"  + computerNumber );
+//  }
+
+// class four................
+// let ice = +prompt("inter your number");
+// if( ice === 100){
+//     console.log("ice-cream is corenetto");
+// }else if( ice === 500){
+//     console.log(".ice-cream is corenetto family pack 1.5 LTR");
+// }else if( ice === 1000){
+//     console.log("ice-cream is corenetto family pack 1.5 LTR");
+// }else{
+//     console.log("your ice-cream is random");
+// }
+
+// let ice = +prompt("inter your number");
+// switch(ice){
+//     case 100:
+//         console.log("ice-cream is corenetto");
+//         break;
+//         case 500:
+//  console.log(".ice-cream is corenetto family pack 1.5 LTR");
+//  break;
+//  case 1000:
+//     console.log("your ice-cream is random");
+//     break;
+//     default:
+//     console.log("your ice-cream is random");
+
+
+//  }
+
+
+
+// let userNum = prompt("Even num");
+// let compNum = Math.round(Math.random() * 10);
+
+// if(compNum % 2 === 0 && userNum === "even"){
+//     console.log("your win !" + compNum);
+// }else if(compNum % 2 !== 0 && userNum === "odd"){
+//     console.log("you win !" + compNum);
+// }else{
+//     console.log("you loss !" + compNum)
+// }
+
+
+
+// initiasation
+// condition
+// increment
+// for(let i = 1; i <= 100 ; i++){
+//     console.log("Hello world" + i)
+// }
+
+// for(let i = 1; i <= 10 ; i++){
+//     console.log("2 x" + i  + " = " + 2 *i)
+// }
+
+
+
+let table = 9;
+for(let i = 1; i <= 20 ; i++){
+        console.log( table + " x" + i  + " = " + table *i)
+        console.log(Math.round(Math.random() * 10));
+        console.log("hellow world");
+     }
+  
